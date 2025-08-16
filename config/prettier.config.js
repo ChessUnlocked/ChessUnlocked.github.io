8f0cd8280a2fa775a439f07c2de29de148f64ae2
Ruslan Sivak <russ@ruslansivak.com>
2025-08-16 17:44:38 -0400

    latest fixes

---
+++ config/prettier.config.js
@@ -0,0 +1,19 @@
+export default {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  embeddedLanguageFormatting: 'auto'
+};
